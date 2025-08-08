cb38aee63785659dc61e8a85c80e4d65df4daab9
son-inchang <3721cha21.11@gmail.com>
2025-08-08 12:57:53 +0900

    feat: Function(defer)

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func lenAndUpper(input string) (length int, uppercase string) {
+	defer fmt.Println("I'm done") // 함수 종료 후 실행됨
 	length = len(input)
 	uppercase = strings.ToUpper(input)
 	return // length, uppercase 가 생략 가능
