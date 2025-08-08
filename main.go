f92305ccbd18775c369d1eb1d5e4fb36f4d7641c
son-inchang <3721cha21.11@gmail.com>
2025-08-08 12:05:07 +0900

    feat: Packages and Imports

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/inchangson/go-for-beginners-by-nomad/something"
+)
 
 func main() {
 	fmt.Println("Hello world!!!")
+	something.SayHello()
+	// something.sayBye() // cannot use as it is private
 }
