ca982a733e6d1c5e5978249c2ff244ff83d3d056
son-inchang <3721cha21.11@gmail.com>
2025-08-08 14:01:22 +0900

    feat: Pointers

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-func isWeekend() {
-	switch time.Now().Weekday() {
-	case time.Saturday, time.Sunday:
-		fmt.Println("Weekend !!!")
-	default:
-		fmt.Println("I'm sorry")
-	}
-}
+import "fmt"
 
 func main() {
-	isWeekend()
+	a := 2
+	b := a
+	fmt.Println("print memory address of a, b", &a, &b)
+	fmt.Println("print values of a, b before change a", a, b)
+	a = 10
+	fmt.Println("print values of a, b after change a", a, b)
+
+	c := &a
+	fmt.Println("print memory addrss of a and value of c", &a, c)
+	fmt.Println("print values of a, *c before change a", a, *c)
+	a = 11
+	fmt.Println("print values of a, *c after change a", a, *c)
+
+	fmt.Println("print values of a, *c before change *c", a, *c)
+	*c = 12
+	fmt.Println("print values of a, *c after change *c", a, *c)
+
 }
