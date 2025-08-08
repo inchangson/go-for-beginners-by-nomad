94983bd0d12443b2df672a2c2494c220f77b0c1f
son-inchang <3721cha21.11@gmail.com>
2025-08-08 12:13:53 +0900

    feat: Variables and Constants

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/inchangson/go-for-beginners-by-nomad/something"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello world!!!")
-	something.SayHello()
-	// something.sayBye() // cannot use as it is private
+	const name string = "nicolas"
+	// name = "Lynn" // cannot change const
+	fmt.Println(name)
+
+	var age int = 30
+	fmt.Println("before", age)
+	age = 31 // can change var
+	fmt.Println("after", age)
+
+	weight := 70.5 // support type inference only if ⭐️it is in the function block⭐️
+	// => var weight float32 = 70.5
+	fmt.Println(weight, "kg")
 }
