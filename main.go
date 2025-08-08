c68948d7ca5c0a969497802222eb094a39ea7314
son-inchang <3721cha21.11@gmail.com>
2025-08-08 14:03:40 +0900

    feat: array

--- main.go
+++ main.go
@@ -3,21 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	a := 2
-	b := a
-	fmt.Println("print memory address of a, b", &a, &b)
-	fmt.Println("print values of a, b before change a", a, b)
-	a = 10
-	fmt.Println("print values of a, b after change a", a, b)
-
-	c := &a
-	fmt.Println("print memory addrss of a and value of c", &a, c)
-	fmt.Println("print values of a, *c before change a", a, *c)
-	a = 11
-	fmt.Println("print values of a, *c after change a", a, *c)
-
-	fmt.Println("print values of a, *c before change *c", a, *c)
-	*c = 12
-	fmt.Println("print values of a, *c after change *c", a, *c)
-
+	names := [5]string{"tony", "jenn", "tim"}
+	names[3] = "ken"
+	names[4] = "austin"
+	fmt.Println(names)
 }
