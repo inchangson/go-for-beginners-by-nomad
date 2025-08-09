77f9d610a33d1cf5897dae9361c7a5b0b46ea7ae
son-inchang <3721cha21.11@gmail.com>
2025-08-09 15:34:30 +0900

    feat: [banking] method using receiver

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,5 +1,7 @@
 package accounts
 
+import "fmt"
+
 // Account struct
 type Account struct { // 대문자로 시작해야 외부에서 접근 가능
 	owner   string
@@ -10,4 +12,32 @@ type Account struct { // 대문자로 시작해야 외부에서 접근 가능
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
+}
+
+// Deposit x amount on your account
+func (targetAccount Account) DepositByValueReceiver(amount int) {
+	// ㄴ reciever: should start with lower case
+	fmt.Println("START[DepositByValueReceiver]======")
+	fmt.Println(targetAccount)
+	targetAccount.balance += amount
+	fmt.Println(targetAccount)
+	fmt.Println("END  [DepositByValueReceiver]======")
+}
+
+// Deposit x amount on your account
+func (targetAccount *Account) Deposit(amount int) {
+	// ㄴ reciever: should start with lower case
+	// ㄴ Go에선 object, struct에 대해서 복사하여 값 전달이 이뤄지기 때문에
+	//   주소값으로 전달받아야지 실제 타겟 객체의 값이 바뀐다.
+	fmt.Println("START[Deposit]======")
+	fmt.Println(targetAccount)
+	targetAccount.balance += amount
+	fmt.Println(targetAccount)
+	fmt.Println("END  [Deposit]======")
+}
+
+// Balance of your account
+func (a Account) Balance() int {
+	//  ㄴ 즉 getter처럼 기존 값 변경 막기 위해선 value receiver로 충분하다
+	return a.balance
 }
--- main.go
+++ main.go
@@ -11,6 +11,16 @@ func main() {
 	// account := banking.Account{owner: "nico", balance: 1000}
 	account := accounts.NewAccount("nico")
 	fmt.Println(account)
+	// private field 까지 출력되지만 이건 fmt에서 reflection을 통해 출력되는 것
+	// 따라서 외부 시스템 혹은 JSON으로 내보낼 땐 공개 필드만 serialize 됨
 	fmt.Println(*account)
+
+	fmt.Println("exec DepositByValueReceiver(100)")
+	account.DepositByValueReceiver(100)
+	fmt.Println(" -> balance:", account.Balance())
+
+	fmt.Println("exec Deposit(100)")
+	account.Deposit(100)
+	fmt.Println(" -> balance:", account.Balance())
 
 }
