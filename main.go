097fbec35437ef4d51e9e5f92afb0dc2bce758bd
son-inchang <3721cha21.11@gmail.com>
2025-08-12 22:29:52 +0900

    test: [banking] 468984b32073e74d5bf2c2f9ef7f59aa10b86c22

--- main.go
+++ main.go
@@ -16,6 +16,9 @@ func main() {
 	// 따라서 외부 시스템 혹은 JSON으로 내보낼 땐 공개 필드만 serialize 됨
 	fmt.Println(*account)
 
+	fmt.Println(account.Balance(), account.Owner())
+	fmt.Println(account)
+
 	fmt.Println("exec DepositByValueReceiver(100)")
 	account.DepositByValueReceiver(100)
 	fmt.Println(" -> balance:", account.Balance())
